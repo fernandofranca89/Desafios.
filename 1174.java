a65db887e787b2b5d88e6a438ccd23a16eddab14
Fernando França <63021925+fernandofranca89@users.noreply.github.com>
2020-06-07 21:24:28 -0300

    Update 1174.java

--- 1174.java
+++ 1174.java
@@ -8,6 +8,7 @@ public class Main {
 
         public static void main(String[] args) throws IOException {
             Scanner leitor = new Scanner(System.in);
+                System.out.println("Digite os valores");
             double[] A = new double[100];
             for (int i=0; i < A.length; i++) {
                 A[i] = leitor.nextDouble();
